b39a16f72715716574370b0bae0aedd14919fb2c
vishal <vp461365@gmail.com>
2024-12-17 16:06:08 +0530

    latest

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://skill11.onrender.com",
     credentials: true
 }))
 
